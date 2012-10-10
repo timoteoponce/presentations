eacc535775482c04303973a3f1dd555940516c67
Timoteo Ponce <timo.slack@gmail.com>
2012-10-09 23:50:19 -0400

    Added script for getting all libs

---
+++ seam/libs/getall.sh
@@ -0,0 +1,5 @@
+wget http://www.h2database.com/h2-2012-09-09.zip
+wget http://sourceforge.net/projects/jboss/files/JBoss/JBoss-5.1.0.GA/jboss-5.1.0.GA-jdk6.zip/download
+wget http://sourceforge.net/projects/jboss/files/JBoss%20Seam/2.2.0.GA/jboss-seam-2.2.0.GA.zip/download
+wget http://apache.spinellicreations.com//ant/binaries/apache-ant-1.8.4-bin.zip
+
