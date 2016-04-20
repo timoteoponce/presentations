30d0fb21337e9dd3c055763eabb8bdb24bf5812c
Timoteo Ponce <timoteo.ponce@swissbytes.ch>
2016-04-20 14:03:21 -0400

    Added missing presentations

--- 2015/2015-11-28_utepsa/hello.go
+++ 2015/2015-11-28_utepsa/hello.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hola!!!")
+	var name string = "Marines"
+	var name2 string = "PAscal"
+	name3 := "timo"
+	fmt.Println(name)
+	fmt.Println(name2)
+	fmt.Println(name3)
+
 }
